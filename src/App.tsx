29a948a1efd2a04ee4495e9ba19cf2a5e8a78350
Angela Patino <angefraugela@gmail.com>
2025-02-02 18:56:48 +0100

    build: Add StrictMode component to check for errors during Dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { StrictMode } from 'react';
 import { Tree } from './tree/Tree';
 import { TreeProvider } from './tree/context/TreeContext';
 
 export function App() {
   return (
-    <TreeProvider>
-      <div className="border-2 border-black flex flex-col">
-        <header className="h-[100px] border-b-2 border-black flex items-center">
-          <h1 className="text-xl font-bold p-4">Home Assignment</h1>
-        </header>
-        <main className="flex flex-1">
-          <Tree />
-        </main>
-      </div>
-    </TreeProvider>
+    <StrictMode>
+      <TreeProvider>
+        <div className="border-2 border-black flex flex-col">
+          <header className="h-[100px] border-b-2 border-black flex items-center">
+            <h1 className="text-xl font-bold p-4">Home Assignment</h1>
+          </header>
+          <main className="flex flex-1">
+            <Tree />
+          </main>
+        </div>
+      </TreeProvider>
+    </StrictMode>
   );
 }
